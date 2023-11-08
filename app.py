8fc7cbb7b34d1cf9ad7bbbfa1ae82d8f15aaf946
JoChan16 <93535186+JoChan16@users.noreply.github.com>
2023-11-08 16:56:51 +0000

    CONTENT

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ completion = client.chat.completions.create(
 
 full_response = client.chat.completions.create(
         model=st.session_state["openai_model"],
-        messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]).choices[0].message.content
+        messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]).choices[0].message.content.strip()
 
 st.session_state.messages.append({"role": "assistant", "content": full_response})
 
@@ -69,34 +69,34 @@ for username, message in reversed(st.session_state.messages):
     with st.chat_message(username):
         st.write(message)
 
-# Creating a sample dataframe for the table
-data = pd.DataFrame({
-    'bucket': ['1D', '1IMM', '2IMM', '3IMM'],
-    'risk': np.random.randn(4),
-    'moves': np.random.randn(4),
-    'P&L': np.random.randint(1000, size=4)
-})
+# # Creating a sample dataframe for the table
+# data = pd.DataFrame({
+#     'bucket': ['1D', '1IMM', '2IMM', '3IMM'],
+#     'risk': np.random.randn(4),
+#     'moves': np.random.randn(4),
+#     'P&L': np.random.randint(1000, size=4)
+# })
 
-# Display the table
-st.table(data)
+# # Display the table
+# st.table(data)
 
-# Create a sample forward rates plot
-forward_rates = pd.DataFrame({
-    'Date': pd.date_range(start='2024', periods=5, freq='Y'),
-    'Forward Rates': np.random.rand(5)
-})
+# # Create a sample forward rates plot
+# forward_rates = pd.DataFrame({
+#     'Date': pd.date_range(start='2024', periods=5, freq='Y'),
+#     'Forward Rates': np.random.rand(5)
+# })
 
-fig = px.line(forward_rates, x='Date', y='Forward Rates', title='Daily Forward Rates from the Yield Curve')
-st.plotly_chart(fig)
+# fig = px.line(forward_rates, x='Date', y='Forward Rates', title='Daily Forward Rates from the Yield Curve')
+# st.plotly_chart(fig)
 
-# Input forms
-with st.form(key='my_form'):
-    notional = st.number_input(label='Enter Notional', value=1000000)
-    effective_date = st.date_input(label='Effective Date')
-    maturity_date = st.date_input(label='Maturity Date')
-    coupon = st.number_input(label='Coupon', value=5.0)
-    submit_button = st.form_submit_button(label='Book')
+# # Input forms
+# with st.form(key='my_form'):
+#     notional = st.number_input(label='Enter Notional', value=1000000)
+#     effective_date = st.date_input(label='Effective Date')
+#     maturity_date = st.date_input(label='Maturity Date')
+#     coupon = st.number_input(label='Coupon', value=5.0)
+#     submit_button = st.form_submit_button(label='Book')
 
-# You can add callbacks or database interactions on button click
-if submit_button:
-    st.write('Transaction booked.')
+# # You can add callbacks or database interactions on button click
+# if submit_button:
+#     st.write('Transaction booked.')
